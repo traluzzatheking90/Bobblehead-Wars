dd4c852f5adef7116dfc52c9e303f3472fd29450
traluzzatheking90 <alessandro.malinverni@live.it>
2017-03-13 01:31:03 +0100

    Added: Game motion, game colliders and cemra sett

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject followTarget;
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(followTarget != null){
+			// Lerp = Linear Interpolator(start position, end position, number between 0 and 1) -> returns a point between two vectors determinate by last number
+			transform.position = Vector3.Lerp(transform.position,followTarget.transform.position,Time.deltaTime * moveSpeed);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position; //Corrent position object
+		pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+		pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+		transform.position = pos;
+	}
+}
