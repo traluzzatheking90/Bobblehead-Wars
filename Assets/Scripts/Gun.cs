3cec0bc8430124bb843d9f1a7377421142ede645
traluzzatheking90 <alessandro.malinverni@live.it>
2017-03-16 23:36:18 +0100

    Added Gun with bullet

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	public Transform launchPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			if(!IsInvoking("fireBullet")){
+				InvokeRepeating("fireBullet",0f,0.1f);
+			}
+		}
+		if(Input.GetMouseButtonUp(0)){
+			CancelInvoke("fireBullet");
+		}
+		
+	}
+
+	void fireBullet(){
+		GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+		bullet.transform.position = launchPosition.position;
+		bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 150;
+	}
+}
