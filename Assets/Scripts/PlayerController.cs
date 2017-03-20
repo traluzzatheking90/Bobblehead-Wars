8a1dea81c37752bef62d1962d1ea636c0cbd2391
traluzzatheking90 <alessandro.malinverni@live.it>
2017-03-20 01:22:38 +0100

    Added: Phisic collider, bobblehead

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed = 50.0f;
+	private CharacterController characterController;
+	public Rigidbody head;
 
 	// Use this for initialization
 	void Start () {
-		
+		characterController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos = transform.position; //Corrent position object
-		pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-		pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-		transform.position = pos;
+		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+		characterController.SimpleMove(moveDirection * moveSpeed);
+	}
+
+
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// It's called at consistent intervarls and not subject to frame rate. Any phisic operation
+	/// should be here (as we make on Rigid body)
+	void FixedUpdate(){
+		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizzontal"),0,Input.GetAxis("Vertical"));
+		if (moveDirection == Vector3.zero){
+			//TODO
+		}else{
+			head.AddForce(transform.right * 150, ForceMode.Acceleration);
+		}
 	}
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// OnBecameInvisible is called when the renderer is no longer visible by any camera.
+	void OnBecameInvisible(){
+		Destroy(gameObject);
+	}
+
+	/// <summary>
+	/// OnCollisionEnter is called when this collider/rigidbody has begun
+	/// touching another rigidbody/collider.
+	/// </summary>
+	/// <param name="other">The Collision data associated with this collision.</param>
+	void OnCollisionEnter(Collision other){
+		Destroy(gameObject);
+	}
+}
